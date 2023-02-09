411eddf8e61d160cb8bba5e411ee40ece7b6f815
Hamza1234b <veljanhamza1@gmail.com>
2023-02-09 11:16:46 +0100

    finished

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express")
 
 const app = express()
+app.use(express.urlencoded({ extended: false }))
 
+let usersUsername
 app.get("/", (req, res) => {
   res.send(`<form method="post" action="/username">
   <label>username</label>
   <input type= "text" name="username" />
-  </form>`)
+  </form>
+  <h1>${usersUsername ? usersUsername : "There is no username"}`)
 })
 
 app.post("/username", (req, res) => {
   const enteredUsername = req.body.username
-  console.log(enteredUsername)
+  usersUsername = enteredUsername
+  res.redirect("/")
 })
 
 app.listen(3000)
